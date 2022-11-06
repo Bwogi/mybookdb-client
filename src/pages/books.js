04402b5175537c8c31ceca12046c6208fc3dfd7d
Andrew Bwogi <andrew.bwogi@gmail.com>
2022-11-06 15:32:57 -0800

    data fetched using axios but server denied because no middleware - cors

--- src/pages/books.js
+++ src/pages/books.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-const books = () => {
+const Books = () => {
+	const [books, setBooks] = useState([]);
+
+	useEffect(() => {
+		const fetchAllBooks = async () => {
+			try {
+				const data = await axios.get('http://localhost:8800/books');
+				console.log(data);
+			} catch (error) {
+				console.log(error);
+			}
+		};
+		fetchAllBooks();
+	}, []);
+
 	return <div>the book list page</div>;
 };
 
-export default books;
+export default Books;
