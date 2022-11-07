1f489630add4f31430595cf1e81fc891809baccb
Andrew Bwogi <andrew.bwogi@gmail.com>
2022-11-06 16:42:02 -0800

    on change for inputs and on click for buttons

--- src/pages/add.js
+++ src/pages/add.js
@@ -1,7 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-const add = () => {
-	return <div>add book page</div>;
+const Add = () => {
+	const [book, setBook] = useState({
+		title: '',
+		desc: '',
+		price: null,
+		cover: '',
+	});
+
+	const changeHandler = (e) => {
+		// whenever there is a change in the field, update or set the book variable with this new value
+		setBook((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+	};
+
+	console.log(book);
+
+	return (
+		<div>
+			<h1>Add a book</h1>
+			<input
+				type='text'
+				placeholder='Book title...'
+				onChange={changeHandler}
+				name='title'
+			/>
+			<input
+				type='text'
+				placeholder='Book description...'
+				onChange={changeHandler}
+				name='desc'
+			/>
+			<input
+				type='number'
+				placeholder='Book price...'
+				onChange={changeHandler}
+				name='price'
+			/>
+			<input
+				type='text'
+				placeholder='Book cover...'
+				onChange={changeHandler}
+				name='cover'
+			/>
+		</div>
+	);
 };
 
-export default add;
+export default Add;
